7b1f1519ec252298cddc1b937e1616e5efbe47ac
haymanjoyce <haymanjoyce@gmail.com>
2021-01-04 15:14:34 +0000

    Solved multiple instances of TopLevel class.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 # TODO sketch out app (tkinter, canvas, postscript, convert postscript to other formats)
+# TODO set window sizes based on screen size
 # TODO reading multiple tabs in pandas
-# TODO create log button
+# TODO stop log button creating more than one dialogue and put working into method
 
 from tkinter import Tk, Frame, Canvas, Label, Button, Entry, filedialog, END, BOTH, X, Y, TOP, BOTTOM, LEFT, RIGHT, PhotoImage, DISABLED, StringVar, Menu, Toplevel, RAISED, scrolledtext, Text
 from tkinter.ttk import Style, Button
@@ -10,22 +11,49 @@ import pandas as pd  # requires manual install of openpyxl (xlrd only does xls)
 import os
 import logging
 import logging.config
-import test
+
+
+class App(Tk):
+    def __init__(self):
+        super(App, self).__init__()
+
+        print(f'Screen dimensions (pixels) - W:{self.winfo_screenwidth()} H:{self.winfo_screenheight()}')
+        print(f'Screen dimensions (mm) - W:{self.winfo_screenmmwidth()} H:{self.winfo_screenmmheight()}')
+
+        logging.info(f'Logging initialised.')
+
+        self.geometry(f'{800}x{600}+{560}+{200}')  # w, h, x, y
+        self.minsize(800, 600)
+        self.title("Main")
+        self.wm_iconbitmap("favicon.ico")
+
+        self.filename = None
+
+        self.toolbar = Toolbar(self)
+        self.chart = Chart(self)
+        self.settings = None
+        self.log = None
+
+        self.mainloop()
 
 
 class Toolbar(Frame):
     def __init__(self, parent):
         super(Toolbar, self).__init__(parent)
 
-        self.btn_select = Button(self, text="Select File", command=self.on_select)
-        self.btn_save = Button(self, text="Save As", command=self.on_save, state=DISABLED)
-        self.btn_settings = Button(self, text="Settings", command=self.on_settings)
-        self.btn_run = Button(self, text="Run", command=self.on_run, state=DISABLED)
-        self.btn_log = Button(self, text="View Log", command=self.on_log)
+        self.parent = parent
+
+        self.pack(side=TOP, fill=BOTH, padx=2, pady=(2, 0))
+
         self.lbl_filename = Label(self, text="Select your Excel file")
+        self.btn_log = Button(self, text="View Log", command=self.on_log)
+        self.btn_settings = Button(self, text="Settings", command=self.on_settings)
+        self.btn_save = Button(self, text="Save As", command=self.on_save, state=DISABLED)
+        self.btn_run = Button(self, text="Run", command=self.on_run, state=DISABLED)
+        self.btn_select = Button(self, text="Select File", command=self.on_select)
 
+        # order matters
         self.lbl_filename.pack(side=LEFT, fill=X)
-
         self.btn_log.pack(side=RIGHT, padx=(0, 0))
         self.btn_settings.pack(side=RIGHT, padx=(0, 2))
         self.btn_save.pack(side=RIGHT, padx=(0, 2))
@@ -33,40 +61,40 @@ class Toolbar(Frame):
         self.btn_select.pack(side=RIGHT, padx=(0, 2))
 
     def on_select(self):
-        # we store filename with root because it's needed by other classes
-        root.filename = filedialog.askopenfilename(initialdir="/desktop", title="Select file", filetypes=(("Excel files (*.xls*)", "*.xls*"), ))
+        # we store filename at root level because it's needed by other classes
+        self.parent.filename = filedialog.askopenfilename(initialdir="/desktop", title="Select file", filetypes=(("Excel files (*.xls*)", "*.xls*"), ))
 
-        self.lbl_filename.configure(text=root.filename)
+        self.lbl_filename.configure(text=self.parent.filename)
 
         if len(self.lbl_filename.cget("text")) != 0:
             self.btn_run.config(state="normal")
             self.btn_save.config(state="normal")
 
     def on_run(self):
-        df = pd.read_excel(root.filename)
+        df = pd.read_excel(self.parent.filename)
         print(df)
         # then check it - logging issues - another toplevel
         # then interpret it - adding to df
         # then parse it
         # then render it - chart.create_line(200, 20, 200, 100, width=1)  # x1, y1, x2, y2
-        chart.itemconfigure(chart.line_a, width=4)
-        chart.create_line(90, 20, 90, 100, width=2)  # x1, y1, x2, y2
+        self.parent.chart.itemconfigure(app.chart.line_a, width=4)
+        self.parent.chart.create_line(90, 20, 90, 100, width=2)  # x1, y1, x2, y2
 
     def on_save(self):
         pass
 
     def on_settings(self):
-
-        settings = Settings()
-
+        settings = Settings(self.parent)
         with open("config.txt", "r") as file:
             data = file.readline()
-
         settings.ent_width.insert(0, data)
 
     def on_log(self):
-        log = Log()
-        log.focus()
+        # kill any old instances if this class
+        if isinstance(self.parent.log, Log):
+            self.parent.log.destroy()
+        self.parent.log = Log(self.parent)
+        self.parent.log.populate()
 
 
 class Chart(Canvas):
@@ -74,6 +102,7 @@ class Chart(Canvas):
         super(Chart, self).__init__(parent)
 
         self.configure(bg="#dddddd")
+        self.pack(fill=BOTH, expand=True)
 
         self.line_a = self.create_line(20, 20, 20, 100, width=1)  # x1, y1, x2, y2
         line_b = self.create_line(20, 20, 80, 20, 80, 100, 140, 100)  # series of x, y points
@@ -82,8 +111,8 @@ class Chart(Canvas):
 
 
 class Settings(Toplevel):
-    def __init__(self):
-        super(Settings, self).__init__()
+    def __init__(self, parent):
+        super(Settings, self).__init__(parent)
 
         self.lbl_width = Label(self, text="Page width:")
         self.ent_width = Entry(self, width=10)
@@ -109,55 +138,29 @@ class Settings(Toplevel):
         # and make any changes to chart object
 
 
-class Handler(logging.Handler):
-
-    def __init__(self, gui):
-        super(Handler, self).__init__()
-        self.gui = gui
-
-    def emit(self, record):
-        msg = self.format(record)
-        self.gui.append(msg)
-
-
 class Log(Toplevel):
-    def __init__(self):
-        super(Log, self).__init__()
+    def __init__(self, parent):
+        super(Log, self).__init__(parent)
 
-        viewer = scrolledtext.ScrolledText(self)
-        self.geometry(f'{400}x{500}+{760}+{250}')  # w, h, x, y
+        self.geometry(f'{500}x{500}+{710}+{250}')  # w, h, x, y
         self.minsize(200, 400)
         self.title("Log")
         self.wm_iconbitmap("favicon.ico")
 
+        self.viewer = scrolledtext.ScrolledText(self)
+        self.viewer.configure(state='disabled')
+        self.viewer.pack(expand=True, fill=BOTH)
+
+    def populate(self):
         with open('app.log', "r") as app_log:
             text = str(app_log.read())
-
-        viewer.configure(state='normal')
-        viewer.insert(END, text)
-        viewer.configure(state='disabled')
-        viewer.pack(expand=True, fill=BOTH)
+        self.viewer.configure(state='normal')  # writable
+        self.viewer.insert(END, text)
+        self.viewer.configure(state='disabled')  # readable
 
 
 if __name__ == '__main__':
 
-    logging.basicConfig(filename='app.log', level=10, filemode='w', format='%(levelname)s | %(module)s | %(message)s')  # 10 is debug, 20 is info
-    logging.info('Logging started.')
+    logging.basicConfig(filename='app.log', level=10, filemode='w', format='%(levelname)s - %(message)s')  # 10 is debug, 20 is info
 
-    root = Tk()
-    logging.info(f'W: {root.winfo_screenwidth()}px H: {root.winfo_screenheight()}px')
-    logging.info(f'W: {root.winfo_screenmmwidth()}mm H: {root.winfo_screenmmheight()}mm')
-
-    root.geometry(f'{800}x{600}+{560}+{200}')  # w, h, x, y
-    root.minsize(800, 600)
-    root.title("Main")
-    root.wm_iconbitmap("favicon.ico")
-    root.filename = None
-
-    toolbar = Toolbar(root)
-    toolbar.pack(side=TOP, fill=BOTH, padx=2, pady=(2, 0))
-
-    chart = Chart(root)
-    chart.pack(fill=BOTH, expand=True)
-
-    root.mainloop()
+    app = App()
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import logging
 
 def testing():
     logging.info(f'This is {__name__} module.')
+
