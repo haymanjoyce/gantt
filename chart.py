7fb54f00e896a55b9bc299b042377f791fa4d0a4
haymanjoyce <haymanjoyce@gmail.com>
2021-02-22 18:36:13 +0000

    Scrollbars added to Chart (but not configured yet).

--- chart.py
+++ chart.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import loggers
-from tkinter import Canvas, BOTH
+from tkinter import Canvas, BOTH, Scrollbar, HORIZONTAL, VERTICAL, X, Y, LEFT, RIGHT, TOP, BOTTOM
 
 
 class Chart(Canvas):
@@ -9,10 +9,30 @@ class Chart(Canvas):
         super(Chart, self).__init__(parent)
 
         self.parent = parent
-        self.settings = parent.get_settings()
 
         self.configure(bg="#dddddd")
+
+        h_bar = Scrollbar(self.parent, orient=HORIZONTAL)
+        h_bar.pack(side=BOTTOM, fill=X)
+        h_bar.config(command=self.xview)
+        v_bar = Scrollbar(self.parent, orient=VERTICAL)
+        v_bar.pack(side=RIGHT, fill=Y)
+        v_bar.config(command=self.yview)
+
+        self.config(width=200, height=200, scrollregion=(0, 0, 400, 2000))
+        self.config(xscrollcommand=h_bar.set, yscrollcommand=v_bar.set)
         self.pack(fill=BOTH, expand=True)
+
+        # canvas = Canvas(frame, bg='#FFFFFF', width=300, height=300, scrollregion=(0, 0, 500, 500))
+        # hbar = Scrollbar(frame, orient=HORIZONTAL)
+        # hbar.pack(side=BOTTOM, fill=X)
+        # hbar.config(command=canvas.xview)
+        # vbar = Scrollbar(frame, orient=VERTICAL)
+        # vbar.pack(side=RIGHT, fill=Y)
+        # vbar.config(command=canvas.yview)
+        # canvas.config(width=300, height=300)
+        # canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        # canvas.pack(side=LEFT, expand=True, fill=BOTH)
 
         self.create_rectangle(0, 0, 1600, 1600, fill="#ff0000")
         self.create_rectangle(0, 0, 800, 800, fill="#0000ff")
--- frames.py
+++ frames.py
@@ -48,7 +48,7 @@ class App(Tk):
         # NO: self.menu = self.menubar
         self.toolbar = Toolbar(self)
         self.viewer = Viewer(self)
-        self.chart = chart.Chart(self)
+        # self.chart = chart.Chart(self)
 
         # LOOP
 
@@ -169,19 +169,9 @@ class Viewer(Frame):
         super(Viewer, self).__init__(parent)
 
         self.parent = parent
+        self.chart = chart.Chart(self)
 
-        # frame = Frame(root, width=300, height=300)
-        # frame.pack(expand=True, fill=BOTH)  # .grid(row=0,column=0)
-        # canvas = Canvas(frame, bg='#FFFFFF', width=300, height=300, scrollregion=(0, 0, 500, 500))
-        # hbar = Scrollbar(frame, orient=HORIZONTAL)
-        # hbar.pack(side=BOTTOM, fill=X)
-        # hbar.config(command=canvas.xview)
-        # vbar = Scrollbar(frame, orient=VERTICAL)
-        # vbar.pack(side=RIGHT, fill=Y)
-        # vbar.config(command=canvas.yview)
-        # canvas.config(width=300, height=300)
-        # canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
-        # canvas.pack(side=LEFT, expand=True, fill=BOTH)
+        self.pack(fill=BOTH, expand=True)
 
 
 cli = loggers.Stream()
