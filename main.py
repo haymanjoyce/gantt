a45acd45407ab4e4eeb183f386cf3ba2960c081f
haymanjoyce <haymanjoyce@gmail.com>
2021-03-12 14:03:28 +0000

    Updated TODO notes after debugging .exe build.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 
 # TODO loading Ghostscript with the programme (ref: https://www.biopdf.com/guide/detecting_ghostscript.php)
 # TODO warning if Ghostscript not installed
+# TODO develop a Windows installer
 # TODO warning if not Windows OS
 # TODO freezing the app and loading along with GhostScript
 # TODO rebuild environment without pyinstaller
 # TODO apply get_path() to all local resources
 # TODO add checking module
-# TODO create copy of gantt and strip down until find issue with pyinstaller
 # TODO select file, press select but don't select file, then run breaks it as file is None
+# TODO solve why data file created on execute; try putting it in log folder or MEIPASS
 
 # MANUAL OS ENVIRONMENT INSTALLS
 # ghostscript
