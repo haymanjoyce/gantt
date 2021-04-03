a7a43b8ee39ea2a5dd5041e32d4cdf18ab39f7e0
haymanjoyce <haymanjoyce@gmail.com>
2021-04-03 21:58:35 +0100

    Changes to template module.

--- source.py
+++ source.py
@@ -27,17 +27,24 @@ def check_header_rows_exist(workbook):
             logging.error(f"Column header(s) missing in {sheet} sheet.")
 
 
-def check_header_names(workbook):
-    for sheet_name in workbook.sheetnames:
-        get_sheet_headers(workbook[sheet_name])
+def check_fieldnames_exist(workbook, mandatory=True):
+    for sheet_name in workbook.sheetnames[:1]:
+        user_headers = get_sheet_fieldnames(workbook[sheet_name])
+        template_sheet = template.TEMPLATE.get(sheet_name)
+        template_sheet_fields = template_sheet.keys()
+        print(user_headers)
+        print(template_sheet_fields)
+        for key in template_sheet_fields:
+            template_mandatory = template_sheet_fields.get("MANDATORY")
+            template_names = template_sheet_fields.get("NAMES")
 
 
-def get_sheet_headers(sheet):
+def get_sheet_fieldnames(sheet):
     return [cell.value for cell in sheet[1][:sheet.max_column]]
 
 
-def get_header_positions(sheet):
-    return [(index, value) for index, value in enumerate(get_sheet_headers(sheet))]
+def get_field_positions(sheet):
+    return [(index, value) for index, value in enumerate(get_sheet_fieldnames(sheet))]
 
 
 def create_template():
