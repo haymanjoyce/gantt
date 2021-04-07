f511001ac8d20f6abd1cbf3683bba3788cbfc361
haymanjoyce <haymanjoyce@gmail.com>
2021-04-07 10:28:58 +0100

    TODO list updated.

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 
+# TODO option for user to email error message back to me
+# TODO better presentation on scroller (line spaces and titles)
 # TODO loading Ghostscript with the programme (ref: https://www.biopdf.com/guide/detecting_ghostscript.php)
 # TODO warning if Ghostscript not installed
 # TODO develop a Windows installer
 # TODO try statement for all processes so breaks gracefully
-# TODO option for user to email error message back to me
-# TODO create sample spreadsheet programmatically
-# TODO create drawing from spreadsheet
-# TODO scroller defaults to bottom so always shows last entry
-# TODO check chart can receive data
-# TODO workbook to dataclass mapping
-# TODO build data objects
-# TODO complete build_export
-# TODO use factory method to build chart
-# TODO put all data objects into dict, not list
-# TODO put col header numbers in dict (name: index)
 # TODO map wb headers to data objects
-# TODO saves as template with default filename
-# TODO in check fieldnames, suggest misspellings
-# TODO add check workbook button
-# TODO check if log has changed
 
 # MANUAL OS ENVIRONMENT INSTALLS
 # ghostscript
