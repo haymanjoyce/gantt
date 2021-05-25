c170e872663550d72cd0b6e0e872d5cf370a1a40
haymanjoyce <haymanjoyce@gmail.com>
2021-05-25 19:50:05 +0100

    Finished set_bars in processing.

--- main.py
+++ main.py
@@ -19,10 +19,9 @@
 # TODO add more scale types (weeks, quarters, half years)
 # TODO use polygon to create box rounding (like https://stackoverflow.com/questions/44099594/how-to-make-a-tkinter-canvas-rectangle-with-rounded-corners)
 # TODO separate calculation from drawing; so all data class values calculated in processing module
-# TODO at cleaning module, removing cleaning from features module
-# TODO auto attrib in features module, minimising annotation
 # TODO sample dates to be interpreted as dates by Excel
 # TODO clean date fields
+# TODO move field cleaning in processing to cleaning
 
 # MANUAL OS ENVIRONMENT INSTALLS
 # ghostscript
--- processing.py
+++ processing.py
@@ -15,6 +15,7 @@ class Processing:
         self.total_days = self.time_delta.days + 1  # range inclusive of end dates
         self.pixels_per_day = self.settings.width / self.total_days
         self.first_row = self.set_scales()
+        self.row_height = self.get_row_height()
         self.set_rows()
         self.set_bars()
 
@@ -39,7 +40,6 @@ class Processing:
             return self.settings.row_height
 
     def set_rows(self):
-        height = self.get_row_height()
         y = self.first_row
         for i in range(0, self.settings.num_rows):
             row = Row()
@@ -47,19 +47,25 @@ class Processing:
             row.x = self.settings.x
             row.y = y
             row.width = self.settings.width
-            row.height = height
-            y += height
+            row.height = self.row_height
+            self.items += row,
+            y += self.row_height
 
     def set_bars(self):
         bars = [item for item in self.items if item.type == 'bar']
         for bar in bars:
+
             # some of this might be better placed in cleaning
             if not isinstance(bar.start, self.settings.start.__class__):
                 bar.start = self.settings.start
             if not isinstance(bar.finish, self.settings.start.__class__):
                 bar.finish = self.settings.start
+
+            # cleaning
             if not int(bar.row):
                 bar.row = 1
+
+            # destined for cleaning
             if bar.start < self.settings.start:
                 bar.start = self.settings.start
             if bar.start > self.settings.finish:
@@ -68,30 +74,25 @@ class Processing:
                 bar.finish = self.settings.start
             if bar.finish > self.settings.finish:
                 bar.finish = self.settings.finish
-        # factor in height
-        # factor in nudge
-        # x
-        # y
 
+            # calculated here, not cleaning
+            if bar.height:
+                if bar.height > self.row_height:
+                    bar.height = self.row_height
 
+            # processing
+            if bar.nudge:
+                if bar.nudge > self.row_height:
+                    bar.nudge = self.row_height
+                if bar.nudge < (self.row_height * -1):
+                    bar.nudge = (self.row_height * -1)
 
+            bar.x = (bar.start - self.settings.start) * self.pixels_per_day
 
-    #
-    # def load_task_locations(self):
-    #     tasks = [item for item in self.items if item.type == 'task']
-    #     unhomed = 0
-    #     for task in tasks:
-    #         if task.row in self.row_locations.keys():
-    #             task.y = self.row_locations[task.row]
-    #         else:
-    #             task.y = self.first_row
-    #             unhomed += 1
-    #     if unhomed:
-    #         logging.warning(f"{unhomed} unhomed task(s) assigned to first row.")
-    #     return tasks
-    #
-    # def draw_tasks(self):
-    #     for task in self.tasks:
-    #         # args = (0, task.y, 100, self.row_height)
-    #         self.draw_rectangle(0, task.y, 100, self.row_height, fill='red', tag='tasks')
-    #     self.tag_lower('tasks')
+            bar.width = (bar.finish - bar.start) * self.pixels_per_day
+
+            # bar y, reference row and then factor nudge
+            bar.y = [item.y for item in self.items if item.type == 'row' if item.key == bar.row][0]
+
+            if bar.nudge:
+                bar.y += bar.nudge
